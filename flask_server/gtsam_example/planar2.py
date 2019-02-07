12695a1d7c39929a22153850aac31d356cb0024e
Jeremy Aguilon <jeraguilon@gmail.com>
2019-02-07 17:31:29 -0500

    Create wrapper object for request

--- flask_server/gtsam_example/planar2.py
+++ flask_server/gtsam_example/planar2.py
@@ -13,116 +13,37 @@ Author: Alex Cunningham (C++), Kevin Deng & Frank Dellaert (Python)
 
 from __future__ import print_function
 
-import numpy as np
-
 import gtsam
 
 from models.example_output import PlanarSlamOutput
+from models.example_input import DEFAULT_REQUEST
 
 
 # Create an empty nonlinear factor graph
-def run(
-    prior_noise=[.3, .3, .1],
-    odometry_noise=[.2, .2, .1],
-    measurement_noise=[.1, .2],
-    symbols = {
-        'x1': {
-            'estimate': [-.25, .20, .15],
-            'type': 'pose'
-        },
-        'x2': {
-            'estimate': [2.30, .10, -.20],
-            'type': 'pose'
-        },
-        'x3': {
-            'estimate': [4.10, .10, .10],
-            'type': 'pose'
-        },
-        'l4': {
-            'estimate': [1.80, 2.10],
-            'type': 'point'
-        },
-        'l5': {
-            'estimate': [4.10, 1.80],
-            'type': 'point'
-        }
-    },
-    priors = { 'x1': [0, 0, 0] },
-    between_pose_factors = [
-        {
-            'connections': ['x1', 'x2'],
-            'pose': [2, 0, 0]
-        },
-        {
-            'connections': ['x2', 'x3'],
-            'pose': [2, 0, 0]
-        },
-    ],
-    bearing_range_factors = [
-        {
-            'connections': ['x1', 'l4'],
-            'bearing': 45,
-            'range': np.sqrt(4.0 + 4.0),
-        },
-        {
-            'connections': ['x2', 'l4'],
-            'bearing': 90,
-            'range': 2,
-        },
-        {
-            'connections': ['x3', 'l5'],
-            'bearing': 90,
-            'range': 2,
-        },
-    ]
-
-):
-    prior_noise = gtsam.noiseModel_Diagonal.Sigmas(np.array(prior_noise))
-    odometry_noise = gtsam.noiseModel_Diagonal.Sigmas(np.array(odometry_noise))
-    measurement_noise = gtsam.noiseModel_Diagonal.Sigmas(np.array(measurement_noise))
-
+def run(slam_request=DEFAULT_REQUEST):
     graph = gtsam.NonlinearFactorGraph()
 
     # Create the keys corresponding to unknown variables in the factor graph
-    unknowns = {}
-    for symbol in symbols.keys():
-        assert len(symbol) == 2
-        var = gtsam.symbol(ord(symbol[0]), int(symbol[1]))
-        unknowns[symbol] = var
+    unknowns = slam_request.symbols
 
     # Add a prior on pose X1 at the origin. A prior factor consists of a mean and a noise model
-    for key, value in priors.iteritems():
-        graph.add(gtsam.PriorFactorPose2(unknowns[key], gtsam.Pose2(*value), prior_noise))
+    for prior in slam_request.priors:
+        graph.add(prior)
 
     # Add odometry factors between X1,X2 and X2,X3, respectively
-    for factor in between_pose_factors:
-        var1 = unknowns[factor['connections'][0]]
-        var2 = unknowns[factor['connections'][1]]
-        graph.add(gtsam.BetweenFactorPose2(
-            var1, var2, gtsam.Pose2(*factor['pose']), odometry_noise))
+    for factor in slam_request.between_pose_factors:
+        graph.add(factor)
 
-    for factor in bearing_range_factors:
-        var1 = unknowns[factor['connections'][0]]
-        var2 = unknowns[factor['connections'][1]]
-        bearing = factor['bearing']
-        range = factor['range']
-        graph.add(gtsam.BearingRangeFactor2D(
-            var1, var2, gtsam.Rot2.fromDegrees(bearing), range, measurement_noise))
+    for factor in slam_request.bearing_range_factors:
+        graph.add(factor)
 
     # Print graph
     print("Factor Graph:\n{}".format(graph))
 
     # Create (deliberately inaccurate) initial estimate
     initial_estimate = gtsam.Values()
-    for key, value in symbols.iteritems():
-        variable = unknowns[key]
-        if value['type'] == 'pose':
-            pose = gtsam.Pose2(*value['estimate'])
-        elif value['type'] == 'point':
-            pose = gtsam.Point2(*value['estimate'])
-        else:
-            raise Exception("Invalid type")
-        initial_estimate.insert(variable, pose)
+    for variable, estimate in slam_request.initial_estimates.iteritems():
+        initial_estimate.insert(variable, estimate)
 
     # Print
     print("Initial Estimate:\n{}".format(initial_estimate))
--- flask_server/models/example_input.py
+++ flask_server/models/example_input.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 import gtsam
+import numpy as np
+
 
 class SymbolType(Enum):
     POSE = "pose"
@@ -32,7 +34,7 @@ class BetweenFactorRequest(object):
             self.odometry_noise,
         )
 
-class BearingFactorRequest(object):
+class BearingRangeFactorRequest(object):
     def __init__(self, var1, var2, bearing, range, measurement_noise):
         self.var1 = var1
         self.var2 = var2
@@ -52,22 +54,153 @@ class BearingFactorRequest(object):
 class SLAMRequest(object):
     def __init__(
         self,
-        prior_noise,
-        odometry_noise,
-        measurement_noise,
         symbols,
         priors,
         between_pose_factors,
         bearing_range_factors,
+        initial_estimates,
     ):
-        self.prior_noise = prior_noise
-        self.odometry_noise = odometry_noise
-        self.measurement_noise = measurement_noise,
         self.symbols = symbols
-        self.prior = priors
+        self.priors = priors
         self.between_pose_factors = between_pose_factors
         self.bearing_range_factors = bearing_range_factors
+        self.initial_estimates = initial_estimates
 
     @staticmethod
     def from_request(request):
-        pass
+        prior_noise = request['priorNoise']
+        prior_noise = gtsam.noiseModel_Diagonal.Sigmas(
+            np.array(prior_noise))
+
+        odometry_noise = request['odometryNoise']
+        odometry_noise = gtsam.noiseModel_Diagonal.Sigmas(
+            np.array(odometry_noise))
+
+        measurement_noise = request['measurementNoise']
+        measurement_noise = gtsam.noiseModel_Diagonal.Sigmas(
+            np.array(measurement_noise))
+
+        symbols = request['symbols']
+
+        unknowns = {}
+        for symbol in symbols.keys():
+            assert len(symbol) == 2
+            var = gtsam.symbol(ord(symbol[0]), int(symbol[1]))
+            unknowns[symbol] = var
+
+        priors_request = request['priors']
+        priors = []
+        for key, value in priors_request.iteritems():
+            priors.append(
+                gtsam.PriorFactorPose2(unknowns[key], gtsam.Pose2(*value), prior_noise)
+            )
+
+        between_pose_factors_request = request['betweenPoseFactors']
+        between_pose_factors = []
+        for factor in between_pose_factors_request:
+            var1 = unknowns[factor['connections'][0]]
+            var2 = unknowns[factor['connections'][1]]
+            pose = factor['pose']
+            factor = BetweenFactorRequest(
+                var1=var1,
+                var2=var2,
+                pose=pose,
+                odometry_noise=odometry_noise
+            ).get_factor()
+            between_pose_factors.append(factor)
+
+
+        bearing_range_factors_request = request['bearingRangeFactors']
+        bearing_range_factors = []
+        for factor in bearing_range_factors_request:
+            var1 = unknowns[factor['connections'][0]]
+            var2 = unknowns[factor['connections'][1]]
+            bearing = factor['bearing']
+            range = factor['range']
+            factor = BearingRangeFactorRequest(
+                var1=var1,
+                var2=var2,
+                bearing=bearing,
+                range=range,
+                measurement_noise=measurement_noise,
+            ).get_factor()
+            bearing_range_factors.append(factor)
+
+        initial_estimates = {}
+        for key, value in symbols.iteritems():
+            variable = unknowns[key]
+            if value['type'] == 'pose':
+                estimate = gtsam.Pose2(*value['estimate'])
+            elif value['type'] == 'point':
+                estimate = gtsam.Point2(*value['estimate'])
+            else:
+                raise Exception("Invalid type")
+            initial_estimates[variable] = estimate
+
+        return SLAMRequest(
+            unknowns,
+            priors,
+            between_pose_factors,
+            bearing_range_factors,
+            initial_estimates
+        )
+
+DEFAULT_ARGS_DICT = {
+    'priorNoise': [.3, .3, .1],
+    'odometryNoise': [.2, .2, .1],
+    'measurementNoise': [.1, .2],
+    'symbols': {
+        'x1': {
+            'estimate': [-.25, .20, .15],
+            'type': 'pose'
+        },
+        'x2': {
+            'estimate': [2.30, .10, -.20],
+            'type': 'pose'
+        },
+        'x3': {
+            'estimate': [4.10, .10, .10],
+            'type': 'pose'
+        },
+        'l4': {
+            'estimate': [1.80, 2.10],
+            'type': 'point'
+        },
+        'l5': {
+            'estimate': [4.10, 1.80],
+            'type': 'point'
+        }
+    },
+    'priors': { 'x1': [0, 0, 0] },
+    'betweenPoseFactors': [
+        {
+            'connections': ['x1', 'x2'],
+            'pose': [2, 0, 0]
+        },
+        {
+            'connections': ['x2', 'x3'],
+            'pose': [2, 0, 0]
+        },
+    ],
+    'bearingRangeFactors': [
+        {
+            'connections': ['x1', 'l4'],
+            'bearing': 45,
+            'range': np.sqrt(4.0 + 4.0),
+        },
+        {
+            'connections': ['x2', 'l4'],
+            'bearing': 90,
+            'range': 2,
+        },
+        {
+            'connections': ['x3', 'l5'],
+            'bearing': 90,
+            'range': 2,
+        },
+    ]
+}
+
+DEFAULT_REQUEST = SLAMRequest.from_request(
+    DEFAULT_ARGS_DICT
+)
