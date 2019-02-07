ea216f3a0e6c52e2a83d9a2a92fa4ab5b3c03e92
Jeremy Aguilon <jeraguilon@gmail.com>
2019-02-07 13:41:48 -0500

    Add flask restful

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
 from gtsam_example import planar2
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def gtsam():
-    return jsonify(planar2.run().serialize())
+
+class SlamExample(Resource):
+    def get(self):
+        return planar2.run().serialize()
+
+api.add_resource(SlamExample, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
