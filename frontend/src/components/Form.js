62fb7124a2866d4e796f5afd197b7ac2d95543fc
Jeremy Aguilon <jeraguilon@gmail.com>
2019-02-10 04:14:35 -0500

    Init form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -6,24 +6,156 @@ import { Form, Field } from 'react-final-form';
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const onSubmit = async values => {
-    await sleep(300);
-    window.alert(JSON.stringify(values, 0, 2));
-}
 
 const INITIAL_VALUES =  {
-    'priors': [
-        {
-            'key': 'x1',
-        }
-    ]
-}
+   "priorNoise":[
+      0.3,
+      0.3,
+      0.1
+   ],
+   "odometryNoise":[
+      0.2,
+      0.2,
+      0.1
+   ],
+   "measurementNoise":[
+      0.1,
+      0.2
+   ],
+   "symbols":[
+      {
+         "estimate":[
+            -0.25,
+            0.2,
+            0.15
+         ],
+      },
+      {
+         "estimate":[
+            2.3,
+            0.1,
+            -0.2
+         ],
+      },
+      {
+         "estimate":[
+            4.1,
+            0.1,
+            0.1
+         ],
+      },
+      {
+         "estimate":[
+            1.8,
+            2.1
+         ],
+      },
+      {
+         "estimate":[
+            4.1,
+            1.8
+         ],
+      }
+   ],
+   "priors":[
+      {
+         "key":"x1",
+         "prior":[
+            0,
+            0,
+            0
+         ]
+      }
+   ],
+   "betweenPoseFactors":[
+      {
+         "connections":[
+            "x1",
+            "x2"
+         ],
+         "pose":[
+            2,
+            0,
+            0
+         ]
+      },
+      {
+         "connections":[
+            "x2",
+            "x3"
+         ],
+         "pose":[
+            2,
+            0,
+            0
+         ]
+      }
+   ],
+   "bearingRangeFactors":[
+      {
+         "connections":[
+            "x1",
+            "l4"
+         ],
+         "bearing":45,
+         "range":2.8284
+      },
+      {
+         "connections":[
+            "x2",
+            "l4"
+         ],
+         "bearing":90,
+         "range":2
+      },
+      {
+         "connections":[
+            "x3",
+            "l5"
+         ],
+         "bearing":90,
+         "range":2
+      }
+   ]
+};
 
 
 
 class SlamForm extends Component {
     async onValidate(values) {
         this.props.formUpdateCallback(values);
+        return [];
+    }
+    onSubmit(values) {
+        for (let i = 0; i < values.symbols.length; i++) {
+            let key = "";
+            let type = "";
+            if (values.symbols[i].estimate.length === 3) {
+                key = "x" + (i + 1).toString();
+                type = "pose";
+            } else {
+                key = "l" + (i + 1).toString();
+                type = "point";
+            }
+            values.symbols[i].key = key
+            values.symbols[i].type = type
+        }
+        console.log("SUBMITTING");
+        console.log(values);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'http://localhost:5000', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.responseType = 'json'
+        let this_props = this.props
+        request.onload = function(e) {
+            if (request.status < 400) {
+                this_props.formSubmitCallback(this.response);
+            } else {
+                alert(request.status.toString() + " error: " + JSON.stringify(this.response));
+            }
+        }
+        request.send(JSON.stringify(values));
     }
 
     constructor(props) {
@@ -36,13 +168,16 @@ class SlamForm extends Component {
         };
         this.addCount = this.addCount.bind(this);
         this.onValidate = this.onValidate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     render() {
+        let poses = this.getPoses();
+        let landmarks = this.getLandmarks(poses.length);
         return (
           <Form
             initialValues={INITIAL_VALUES}
-            onSubmit={onSubmit}
+            onSubmit={this.onSubmit}
             validate={this.onValidate}
             render={({ handleSubmit, pristine, invalid }) => (
               <form onSubmit={handleSubmit}>
@@ -68,8 +203,8 @@ class SlamForm extends Component {
 
 
                 <h4>Variables and Initial Estimates</h4>
-                {this.getPoses()}
-                {this.getLandmarks()}
+                {poses}
+                {landmarks}
                 <button type="button" onClick={(e) => this.addCount('numPoses')}>Add Pose</button>
                 <button type="button" onClick={(e) => this.addCount('numLandmarks')}>Add Landmark</button>
 
@@ -91,7 +226,7 @@ class SlamForm extends Component {
                 <button type="button" onClick={(e) => this.addCount('numMeasurementFactors')}>Add Measurement Factor</button>
 
                 <div>
-                    <button type="submit" disabled={pristine || invalid}>
+                    <button type="submit" disabled={invalid}>
                       Submit
                     </button>
                 </div>
@@ -112,8 +247,8 @@ class SlamForm extends Component {
         let numBetweenFactors = this.state.numBetweenFactors;
         let output = []
         for (let i = 0; i < numBetweenFactors; i++) {
-            let varId = i + 1;
-            let name = 'betweeenPoseFactors[' + varId.toString() + ']';
+            let varId = i;
+            let name = 'betweenPoseFactors[' + varId.toString() + ']';
             let new_component = (
                 <div key={i.toString()}>
                   <Field name={name + ".connections[0]"} component="input" placeholder="var1" />
@@ -133,7 +268,7 @@ class SlamForm extends Component {
         let numMeasurementFactors = this.state.numMeasurementFactors;
         let output = []
         for (let i = 0; i < numMeasurementFactors; i++) {
-            let varId = i + 1;
+            let varId = i;
             let name = 'bearingRangeFactors.[' + varId.toString() + ']';
             let new_component = (
                 <div key={i.toString()}>
@@ -158,14 +293,14 @@ class SlamForm extends Component {
         let numPoses = this.state.numPoses;
         let output = []
         for (let i = 0; i < numPoses; i++) {
-            let varId = i + 1;
+            let varId = i;
             let name = 'symbols[' + varId.toString() + ']';
             let new_component = (
                 <div key={i.toString()}>
-                  <label>{name}</label>
-                  <Field name={name + "X"} component="input" placeholder="x" />
-                  <Field name={name + "Y"} component="input" placeholder="y" />
-                  <Field name={name + "Theta"} component="input" placeholder="theta" />
+                  <label>x{i + 1}</label>
+                  <Field name={name + ".estimate[0]"} component="input" placeholder="x" />
+                  <Field name={name + ".estimate[1]"} component="input" placeholder="y" />
+                  <Field name={name + ".estimate[2]"} component="input" placeholder="theta" />
                 </div>
 
             );
@@ -174,17 +309,17 @@ class SlamForm extends Component {
         return output;
     }
 
-    getLandmarks() {
+    getLandmarks(initialI) {
         let numLandmarks = this.state.numLandmarks;
         let output = []
-        for (let i = 0; i < numLandmarks; i++) {
-            let varId = i + 1;
-            let name = 'l' + varId.toString();
+        for (let i = initialI; i < initialI + numLandmarks; i++) {
+            let varId = i;
+            let name = 'symbols[' + varId.toString() + ']';
             let new_component = (
                 <div key={i.toString()}>
-                  <label>{name}</label>
-                  <Field name={name + "X"} component="input" placeholder="x" />
-                  <Field name={name + "Y"} component="input" placeholder="y" />
+                  <label>l{i+1}</label>
+                  <Field name={name + ".estimate[0]"} component="input" placeholder="x" />
+                  <Field name={name + ".estimate[1]"} component="input" placeholder="y" />
                 </div>
 
             );
